e55829e238aef6df23f0d8a2719fd706565c1386
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-06-03 15:55:04 +0300

    fleks

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,1 +1,32 @@
-export { default } from './page';
+
+
+import Link from "next/link";
+
+export default function Sidebar() {
+  return (
+    <aside>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/notes">All notes</Link>
+          </li>
+          <li>
+            <Link href="/notes?tag=Todo">Todo</Link>
+          </li>
+          <li>
+            <Link href="/notes?tag=Work">Work</Link>
+          </li>
+          <li>
+            <Link href="/notes?tag=Personal">Personal</Link>
+          </li>
+          <li>
+            <Link href="/notes?tag=Meeting">Meeting</Link>
+          </li>
+          <li>
+            <Link href="/notes?tag=Shopping">Shopping</Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
