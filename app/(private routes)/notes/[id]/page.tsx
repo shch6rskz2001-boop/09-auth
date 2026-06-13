6de658b600e4d48cdbd5863a37b6ffe4f80bc6dc
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-06-13 20:12:21 +0300

    fleks

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { fetchNoteById } from '../../../lib/api';
+import { fetchNoteById } from '../../lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 
 interface Props {
