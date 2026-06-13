f10e5e24b765c8574b64c7c8f2738bfe48c34484
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-06-13 16:57:16 +0300

    fleks

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,44 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { checkSession } from '@/lib/api/serverApi';
 
 const privateRoutes = ['/profile', '/notes'];
 const publicRoutes = ['/sign-in', '/sign-up'];
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+
   const isPrivate = privateRoutes.some((route) => pathname.startsWith(route));
   const isPublic = publicRoutes.some((route) => pathname.startsWith(route));
 
-  const user = await checkSession();
-  const isAuthenticated = !!user;
+  
+  const cookieHeader = request.headers.get('cookie') ?? '';
+
+  let isAuthenticated = false;
+
+  try {
+    const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
+    const response = await fetch(`${baseURL}/auth/session`, {
+      headers: {
+        Cookie: cookieHeader,
+      },
+      credentials: 'include',
+    });
+
+    if (response.ok) {
+      const data = await response.json().catch(() => null);
+      isAuthenticated = !!data;
+    }
+  } catch {
+    isAuthenticated = false;
+  }
 
   if (isPrivate && !isAuthenticated) {
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
+
   if (isPublic && isAuthenticated) {
     return NextResponse.redirect(new URL('/profile', request.url));
   }
+
   return NextResponse.next();
 }
 
