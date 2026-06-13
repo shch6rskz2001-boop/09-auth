f981a50c24605f48d73a9eb35eb9e12a9981f0a4
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-06-13 22:22:21 +0300

    fleks

--- proxy.ts
+++ proxy.ts
@@ -3,13 +3,12 @@ import { NextRequest, NextResponse } from 'next/server';
 const privateRoutes = ['/profile', '/notes'];
 const publicRoutes = ['/sign-in', '/sign-up'];
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const isPrivate = privateRoutes.some((route) => pathname.startsWith(route));
   const isPublic = publicRoutes.some((route) => pathname.startsWith(route));
 
-  
   const cookieHeader = request.headers.get('cookie') ?? '';
 
   let isAuthenticated = false;
@@ -17,12 +16,8 @@ export async function middleware(request: NextRequest) {
   try {
     const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
     const response = await fetch(`${baseURL}/auth/session`, {
-      headers: {
-        Cookie: cookieHeader,
-      },
-      credentials: 'include',
+      headers: { Cookie: cookieHeader },
     });
-
     if (response.ok) {
       const data = await response.json().catch(() => null);
       isAuthenticated = !!data;
@@ -42,6 +37,10 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
+export { proxy as middleware };
+
 export const config = {
   matcher: ['/profile/:path*', '/notes/:path*', '/sign-in', '/sign-up'],
-};
+}; 
+
+  
