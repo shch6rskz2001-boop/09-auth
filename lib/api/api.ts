cab342de12efae61d81f9376a4d7ccd82fcb8f7a
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-06-14 17:51:12 +0300

    fleks

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
-
 const api = axios.create({
-  baseURL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
