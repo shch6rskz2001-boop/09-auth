4016ad32b18da140c346765838ca81ba3d2efe3a
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-06-14 18:30:47 +0300

    fix: env url

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
   withCredentials: true,
 });
 
 export default api;
-
 
